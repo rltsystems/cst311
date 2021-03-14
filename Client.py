3b623f13f9404c674fed3db0e35929ad9bca8422
None <None>
2021-03-14 16:35:35 -0700

    added output.txt

--- Client.py
+++ Client.py
@@ -1,29 +1,29 @@
 #Client
 import time
 from socket import *
-​
+
 serverName = 'mininet-vm'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1.0)
-​
+
 baseMessage = "Ping"
 alpha = 0.125
 beta = 0.25 # this value is typically 0.25
 packetsLost = 0.0
 maxRTT = 0
 minRTT = 1000
-​
+
 totalTimeElapsed = 0.0
 avgRTT = 0.0
 estRTT = 0.0
 devRTT = 0.0
 estRTT_prev = 0.0
 devRTT_prev = 0.0
-​
+
 firstSucMess = True
 print()
-​
+
 # send message 10 times
 for i in range(1,11):
     message = baseMessage + str(i)
@@ -38,7 +38,7 @@ for i in range(1,11):
         print("Mesg rcvd: " + rsentence.decode())
         elapsed = (end - start) * 1000
         print("PONG " + str(i) + " RTT: " + str(elapsed) + "ms\n")
-​
+
         if (i == 1): # was if(firstSucMess):
             # uppon first successful rtt measured set the values max, min, est, dev
             maxRTT = elapsed
@@ -53,24 +53,24 @@ for i in range(1,11):
         # save values as previous for the next iteration
         estRTT_prev = estRTT
         devRTT_prev = devRTT
-​
+
         if (elapsed > maxRTT):
             maxRTT = elapsed
         if (elapsed < minRTT):
             minRTT = elapsed
         totalTimeElapsed += elapsed
-​
+
     except timeout:
         print("No Mesg rcvd ")
         print("PONG " + str(i) + " Request Timed out\n")
         packetsLost += 1
         estRTT = estRTT_prev
         devRTT = devRTT_prev
-​
+
 avgRTT = totalTimeElapsed / (10-packetsLost)
 packetsLostPcent = (packetsLost/10) * 100
 timeoutInter = estRTT + (4 * devRTT)
-​
+
 print("Min RTT:\t  " + str(minRTT) + " ms")
 print("Max RTT:\t  " + str(maxRTT) + " ms")
 print("Avg RTT:\t  " + str(avgRTT) + " ms")
